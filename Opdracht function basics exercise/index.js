a7629fc93c7866e2354c43ca89a15ed40128c102
Typosphere <67193118+Typosphere@users.noreply.github.com>
2021-07-07 17:45:40 +0200

    Add files via upload

---
+++ Opdracht function basics exercise/index.js
@@ -0,0 +1,21 @@
+const makePizza = function (){
+    console.log("prepare the dough");
+    console.log("create the sauce");
+    console.log("ready for the oven");
+};
+
+makePizza();
+makePizza();
+
+
+const makeSushi = function (){
+    console.log("boil the water");
+    console.log("prepare the rice");
+    console.log("prepare the tuna and the cumcumber");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
